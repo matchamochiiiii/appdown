6885c31b8c3defeab58b6e0223a1f6cae40a2e0c
Truong My Linh <93811231+matchamochiiiii@users.noreply.github.com>
2023-04-21 23:36:06 +0700

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,82 +1,76 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import messagebox, filedialog
-
-import youtube_dl
+from tkinter.ttk import Progressbar
+from logic import * 
+from tkinter import messagebox
+import time
 
 window = tk.Tk()
-window.geometry('620x125')
+window.geometry('620x180')
 window.title('Download App')
 window.resizable(False, False)
 
 window.columnconfigure(0,weight=1)
 window.rowconfigure(0, weight=1)
 window.rowconfigure(1, weight=1)
+window.rowconfigure(2,weight=1)
 
 linkVideo = StringVar()
 location = StringVar()
 
-frame_top = Frame(window)
-frame_bottom = Frame(window)
+frame_top=Frame(window,bg='#B3E6E8')
+frame_end=Frame(window,bg='#B3E6E8')
+frame_bottom=Frame(window,bg='#B3E6E8')
 
-frame_top.grid(row=0,column=0, sticky='WENS')
-frame_bottom.grid(row=1,column=0, sticky='WENS')
+frame_top.pack(fill=X)
+frame_bottom.pack(fill=X)
+frame_end.pack(fill=X)
 
+pd = Progressbar (
+        frame_end,
+        orient='horizontal',
+        mode="determinate",
+        length=600
+)
+pd.pack(fill=BOTH,padx=5, pady=5)
 
 def GUI():
-    lbURL = Label(
-    frame_top, text='URL',pady=2 ,padx=5)
-    lbURL.grid(row=1, column=0, pady=2 ,padx=20)
-    txtURL = Entry(
-    frame_top, width=66, textvariable=linkVideo)
-    txtURL.grid(row=1, column=1)
+    lbURL = Label(frame_top, text='Video URL:', width=10,bg='#B3E6E8')
+    lbURL.pack(side=LEFT, anchor=N,padx=5, pady=5)
 
-    lbLocation = Label(
-    frame_bottom, text='Location')
-    lbLocation.grid(row=2, column=0, padx=7)
-    txtLocation = Entry(
-    frame_bottom, width=50, textvariable=location )
-    txtLocation.grid(row=2, column=1, padx=5, pady=1)
-    btnLocation = Button(
-    frame_bottom, text = 'Location', width=11, relief=GROOVE,
-    command=LocationFunc)
-    btnLocation.grid(row=2, column=2, pady=1,padx=3)
+    global txtURL
+    txtURL = Entry(frame_top, textvariable=linkVideo)
+    txtURL.pack(fill=BOTH, expand=True,padx=5, pady=5)
 
+    lbLocation = Label(frame_bottom, text='Save to:',width=10,bg='#B3E6E8')
+    lbLocation.pack(side=LEFT, anchor=N,padx=5, pady=5)
+    global txtLocation
+    txtLocation = Entry(frame_bottom, textvariable=location, state=DISABLED,disabledbackground='#FFFFFF')
+    txtLocation.pack(fill=BOTH, expand=True,padx=5, pady=5)
+
+    btnLocation = Button(frame_end, text = 'Browser',relief=RAISED,bg='#B3E6E8',
+    command=lambda: LocationFunc(location))
+    btnLocation.pack(fill=BOTH,padx=5, pady=5)
 
     btnDownload = Button(
-    frame_bottom, text = 'Download', width=11, relief=GROOVE,
-    command=DownloadFunc)
-    btnDownload.grid(row=3, column=1, pady=1,padx=3)
-
-def LocationFunc():
-    global down_Directory
-    down_Directory = filedialog.askdirectory(
-    initialdir ='Home', title = 'Save video')
-    location.set(down_Directory)
-
-def DownloadFunc():
-    '''
-        Cài đặt các option của chrome driver dẫn đến đường dẫn url
-    '''
-    global down_Directory
-
-    # url = 'https://www.youtube.com/watch?v=T_ub4chD8cg'
-    ydl_opts = {
-        'outtmpl': f'{down_Directory}/%(title)s-%(id)s.%(ext)s',
-        'format': 'bestvideo+bestaudio/best',
-        'progress_hooks': [my_hook],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([linkVideo.get()])
+    frame_end, text = 'Download', width=72, relief=RAISED,bg='#B3E6E8',
+    command=lambda: DownloadFunc(linkVideo, location,my_hook))
+    btnDownload.pack(fill=BOTH,padx=5, pady=5)
 
 def my_hook(d):
     if d['status'] == 'finished':
-        messagebox.showinfo("SUCCESSFULLY",
-                          "DOWNLOADED AND SAVED IN")
+        messagebox.showinfo("SUCCESSFULLY", "DOWNLOADED AND SAVED IN")
+        pd['value'] = 0
+    #txtURL.set("")
+    #txtLocation.set("")
+       
+    if d['status'] == 'downloading':
+        pd['value'] += 20
+        window.update_idletasks()
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     linkVideo = StringVar()
-    down_Directory = StringVar()
     GUI()
     window.mainloop()
 
---
+++ logic.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import  filedialog, messagebox
+
+import youtube_dl
+
+def LocationFunc(location):
+    down_Directory = filedialog.askdirectory(
+    initialdir ='Home', title = 'Save video')
+    location.set(down_Directory)
+
+def DownloadFunc(linkVideo,location, my_hook):
+    if linkVideo.get() != '':
+        ydl_opts = {
+            'outtmpl':f'{location.get()}/%(title)s-%(id)s.%(ext)s',
+            'format': 'bestvideo+bestaudio/best',
+            'progress_hooks': [my_hook],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            try:
+                ydl.download([linkVideo.get()])
+            except:
+                messagebox.showinfo("Error", "Link video invalid")
+            
+    else:
+        messagebox.showinfo("Error", "Link video should not be EMPTY")
+
+
+
